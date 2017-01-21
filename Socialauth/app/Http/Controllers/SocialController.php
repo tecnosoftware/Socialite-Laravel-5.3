24a25c37f08f51fdb772c059d0afda62b47e9d2e
jose cedeño <manuel.cesan@gmail.com>
2017-01-21 16:04:25 +0100

    Curso Express Laravel5.3 Socialite

---
+++ Socialauth/app/Http/Controllers/SocialController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Socialite;
+
+class SocialController extends Controller
+{
+    //
+
+    public function redirect ($provider){
+    	return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider){
+    	$user = Socialite::driver($provider)->user();
+
+    	return ($user->name);
+    }
+}
